44f8d2a9fbb280fe2b40c0ff190cb9de0dd35c38
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-09-12 15:47:59 +0200

    Updated console outputs

--- Challenge 3/Challenge f/script.js
+++ Challenge 3/Challenge f/script.js
@@ -1,13 +1,12 @@
 let date = 2050
-let status= 'student'
+let status= 'Student'
 let count = 0
 
 if (date === 2050) {
 	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
+	console.log("April", 'Family Day')
+	console.log('April', 'Freedom Day')
 	count = count + 4
 
 	if (status === "student") {
@@ -17,16 +16,15 @@ if (date === 2050) {
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
+	console.log('December', 'Day of Reconciliation')
 	 count = count + 3
 
 	if (status === "parent") {
-	  console.log(date, 'Christmas Day')
+	  console.log('December', 'Christmas Day')
 		 count = count + 1
   }
 
-	console.log(date, 'Day of Goodwill')
+	console.log('December', 'Day of Goodwill')
 	 count = count + 1
 }
 
