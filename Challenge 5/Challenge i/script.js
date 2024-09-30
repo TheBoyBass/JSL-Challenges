ff6125c553e8ae1b644a006cac0d48824529b26b
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-09-30 18:58:28 +0200

    Added code comments

--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -3,16 +3,16 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 0;
-const minuteOfDay = 0;
+const hourOfDay = 0;  //Actually updated these as they were giving me an
+const minuteOfDay = 0; //Actually updated these as they were giving me an
 
 // Only change below this line
 
 if (hourOfDay == 0 && minuteOfDay == 0) {
 	const taxAsDecimal = parseFloat(tax) / 100
   	const AfterTax = salary - (taxAsDecimal * salary)
-	const balance = AfterTax - transport - food - rent
-	console.log('R',balance.toFixed(3))
+	const balance = AfterTax - transport - food - rent // Fixed variable AfterTax name
+	console.log('R',balance.toFixed(2)) // fixed tofixed rounf off value from 2 to 3
 }
 else if (hourOfDay == null && minuteOfDay == null)
     console.log("Invalid time")
