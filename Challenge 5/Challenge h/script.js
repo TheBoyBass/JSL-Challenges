bd921160fdddf4708ce3746fb990770ee401280f
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-09-30 18:55:30 +0200

    Fixed Both challenge bugs

--- Challenge 5/Challenge h/script.js
+++ Challenge 5/Challenge h/script.js
@@ -4,7 +4,7 @@ const secondaryPhone = '0219131568'
 // Only change below this line
 
 const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const secondaryValid = isNaN == secondaryPhone
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -3,15 +3,17 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (hourOfDay == 0 && minuteOfDay == 0) {
+	const taxAsDecimal = parseFloat(tax) / 100
+  	const AfterTax = salary - (taxAsDecimal * salary)
+	const balance = AfterTax - transport - food - rent
+	console.log('R',balance.toFixed(3))
 }
+else if (hourOfDay == null && minuteOfDay == null)
+    console.log("Invalid time")
 	
-console.log(balance.toFixed(3))
